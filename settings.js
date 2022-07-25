72061162ffb8202abf395dabafb4d7cd3a0f3e40
ANkit9800dsdas <106540086+ANkit9800dsdas@users.noreply.github.com>
2022-07-25 19:02:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
-global.botname = 'BIXBY-MD USER BOT'
-global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
+global.owner = ['916200767046']
+global.premium = ['916200767046']
+global.ownernomer = '916200767046'
+global.ownername = 'ITACHI'
+global.botname = 'Raiden-MD USER BOT'
+global.footer = ' Raiden ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/darkalphaxteam/QUEEN-BIXBY-MD-PRO-NEW'
-global.myweb = 'https://youtube.com/channel/UCvpyOCBCaBSCdv4QolnO0WQ'
-global.packname = 'Queen Bixby MD'
-global.author = 'DARK ALPHA'
+global.region = 'India, North India, bihar'
+global.sc = 'https://github.com/'
+global.myweb = 'https://youtube.com/'
+global.packname = 'Raiden Shogun MD'
+global.author = 'ANKiT'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
